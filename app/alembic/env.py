313110770979e1201ff68ff8ae74fed33ae104e1
nzhussup <nurzhanat2002@gmail.com>
2024-11-03 13:55:29 +0100

    Task 5 completed. What is done: Fixed bugs with db connection. Bugs were introduced because of project refactoring. Added locations unit tests (7 tests).

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -15,7 +15,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app.models import Base
+from app.models.base import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -8,10 +8,12 @@ import alembic.config
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
+from sqlmodel import select
 
-from app.db import get_database_session
+from app.core.db import get_database_session
 from app.main import app
-from app.models import Organisation
+from app.models.organisation import Organisation
+from app.models.location import Location, CreateLocation
 
 _ALEMBIC_INI_PATH = Path(__file__).parent.parent / "alembic.ini"
 
@@ -30,7 +32,7 @@ def apply_alembic_migrations() -> Generator[None, None, None]:
         alembic_cfg.attributes["sqlalchemy_url"] = test_db_url
         alembic.command.upgrade(alembic_cfg, "head")
         test_engine = create_engine(test_db_url, echo=True)
-        with patch("app.db.get_engine") as mock_engine:
+        with patch("app.core.db.get_engine") as mock_engine:
             mock_engine.return_value = test_engine
             yield
     finally:
@@ -42,7 +44,8 @@ def test_organisation_endpoints(test_client: TestClient) -> None:
 
     # Validate that organisations do not exist in database
     with get_database_session() as database_session:
-        organisations_before = database_session.query(Organisation).all()
+        query = select(Organisation)
+        organisations_before = database_session.exec(query).all()
         database_session.expunge_all()
     assert len(organisations_before) == 0
 
@@ -53,7 +56,8 @@ def test_organisation_endpoints(test_client: TestClient) -> None:
 
     # Validate that organisations exist in database
     with get_database_session() as database_session:
-        organisations_after = database_session.query(Organisation).all()
+        query = select(Organisation)
+        organisations_after = database_session.exec(query).all()
         database_session.expunge_all()
     created_organisation_names = set(organisation.name for organisation in organisations_after)
     assert created_organisation_names == set(list_of_organisation_names_to_create)
@@ -62,3 +66,86 @@ def test_organisation_endpoints(test_client: TestClient) -> None:
     response = test_client.get("/api/organisations")
     organisations = set(organisation["name"] for organisation in response.json())
     assert  set(organisations) == created_organisation_names
+    
+    
+    
+def test_location_endpoints(test_client: TestClient) -> None:
+
+    # Create org
+    org_response = test_client.post("/api/organisations/create", json={"name": "Test Organisation"})
+    assert org_response.status_code == status.HTTP_200_OK
+    organisation_id = org_response.json()["id"]
+
+
+    # Test 1: No locs for org
+    with get_database_session() as db_session:
+        locations_before = db_session.exec(select(Location).where(Location.organisation_id == organisation_id)).all()
+        db_session.expunge_all()
+    assert len(locations_before) == 0
+
+
+    
+    # Arrange locs
+    locations_to_create = [
+        {"organisation_id": organisation_id, "location_name": "test 1", "longitude": 123.456, "latitude": 78.910},
+        {"organisation_id": organisation_id, "location_name": "test 2", "longitude": 223.456, "latitude": 88.910}
+    ]
+
+    # Test 2: Validate create locs
+    for location_data in locations_to_create:
+        response = test_client.post("/api/locations/create", json=location_data)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()["location_name"] == location_data["location_name"]
+        
+
+        
+    # Test 3: Validate 404 for no org while creating loc
+    response = test_client.post("/api/locations/create", json={
+        "organisation_id": 5,
+        "location_name": "does not exists",
+        "longitude": 0,
+        "latitude": 0
+    })
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    
+    # Test 4: Validate locations exist in the database
+    with get_database_session() as db_session:
+        locs = db_session.exec(select(Location).where(Location.organisation_id == organisation_id)).all()
+        db_session.expunge_all()
+    created_location_names = set(loc.location_name for loc in locs)
+    assert created_location_names == set(loc["location_name"] for loc in locations_to_create)
+
+
+    # Test 5: Validate length of the retrieved locs for org
+    response = test_client.get(f"/api/locations/{organisation_id}")
+    assert response.status_code == status.HTTP_200_OK
+    retrieved_locations = response.json()
+    assert len(retrieved_locations) == len(locations_to_create)
+
+
+    retrieved_location_names = set(location["location_name"] for location in retrieved_locations)
+    assert retrieved_location_names == created_location_names
+
+    # Test 6: Non existent org
+    response = test_client.get("/api/locations/9999")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    
+
+    # Test 7: Retrieve with bounding box
+    bounding_box = {
+        'bounding_box': [
+            78.900, 
+            123.450,
+            78.920, 
+            123.460 
+        ]
+    }
+    response = test_client.get(f"/api/locations/{organisation_id}", params=bounding_box)
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == 1
+
+
+
+    
